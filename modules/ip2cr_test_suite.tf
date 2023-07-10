80c8e7936b84ecf7bf0403600bf70be130fc9b2b
Josh Carlson <magneticstain@gmail.com>
2023-07-09 21:36:21 -0400

    Add coudfront support
    COMPLETES: https://github.com/magneticstain/tf-ip2cr/issues/3

--- modules/ip2cr_test_suite.tf
+++ modules/ip2cr_test_suite.tf
@@ -1,6 +1,6 @@
 # Generate all resources supported by ip2cr
 
-# At a high level, we want to create an EC2 instance, put an ELB and ALB in front of it, and then attach a newly-created cloudfront distro to the ALB/ELB
+# At a high level, we want to create an EC2 instance, put a NLB and ALB in front of it, and then attach a newly-created cloudfront distro to the ALB
 
 # publicly accessible EC2 instance
 resource "aws_instance" "ip2cr-test" {
@@ -14,7 +14,6 @@ resource "aws_instance" "ip2cr-test" {
   }
 }
 
-# alb
 resource "aws_security_group" "ip2cr-test-sg" {
   name        = "IP2CR-Testing"
   description = "Allow access to EC2 instance for IP2CR testing purposes"
@@ -27,6 +26,7 @@ resource "aws_security_group" "ip2cr-test-sg" {
   }
 }
 
+# alb
 resource "aws_lb" "ip2cr-testing-alb" {
   name               = "IP2CR-Testing-ALB"
   load_balancer_type = "application"
@@ -56,5 +56,51 @@ resource "aws_lb_listener" "ip2cr-testing-alb-listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ip2cr-testing-tg.arn
+  }
+}
+
+# cloudfront
+resource "aws_cloudfront_distribution" "ip2cr-cf-distro" {
+  origin {
+    domain_name = aws_lb.ip2cr-testing-alb.dns_name
+    origin_id   = "ip2cr-alb-origin"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "ip2cr-alb-origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    app = "ip2cr"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
