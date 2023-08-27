7e3ad7cc4ff2d4592e5c9538312026a1d59867b6
Josh Carlson <magneticstain@gmail.com>
2023-08-27 19:49:44 -0400

    Update backeng generation script for multi-account support

--- utils/generate_backend.sh
+++ utils/generate_backend.sh
@@ -5,11 +5,7 @@
 # Thanks ChatGPT :)
 #
 
-PROJ_NAME="ip2cr"
-
-# Set your AWS region and profile
-AWS_REGION="us-east-1"
-AWS_PROFILE="default"  ## update this if needed
+PROJ_NAME="ip2cr-$(date +%s)"
 
 # Set the bucket and table names
 S3_BUCKET_NAME="tf-${PROJ_NAME}"
