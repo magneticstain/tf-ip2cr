fb8d005b1a57b6667cb8805f01dd25ee16fca89b
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-16 16:04:51 -0500

    Add logic for creating dedicated VPC for resources

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,1 @@
 ami_id = "ami-053b0d53c279acc90"  # Ubuntu Server 22.04 LTS
-key_pair_name = "default"
-subnets = []
-vpc = ""
--- variables.tf
+++ variables.tf
@@ -4,24 +4,3 @@ variable "ami_id" {
     default = ""
     nullable = false
 }
-
-variable "key_pair_name" {
-    type = string
-    description = "Name of SSH key pair to use for access to test EC2 instance"
-    default = ""
-    nullable = false
-}
-
-variable "subnets" {
-    type = list
-    description = "IDs of subnets that load balancers should live in"
-    default = []
-    nullable = false
-}
-
-variable "vpc" {
-    type = string
-    description = "VPC that load balancers should live in"
-    default = ""
-    nullable = false
-}
