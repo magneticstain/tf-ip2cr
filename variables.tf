ce1af91de577b1c5d57a8d1bfde4828c08faeb25
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-17 14:32:42 -0500

    Update for usage with single aws account

--- variables.tf
+++ variables.tf
@@ -18,9 +18,3 @@ variable "vpc" {
     default = ""
     nullable = false
 }
-
-variable "accounts" {
-    type = map(string)
-    description = "Mapping of aliases->IAM roles of accounts to rollout plans to"
-    default = {}
-}
